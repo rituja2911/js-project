821e2bd3a55e61a3a7eb6cd2d077277e286f2d1f
Rutuja Mirkale <146189819+rituja2911@users.noreply.github.com>
2025-01-12 19:08:07 +0000

    done variables

---
+++ .devcontainer/01_baiscs/01_vairables.js
@@ -0,0 +1,23 @@
+const accountid = 125869
+let accountemail = "rutuja@gmail.com"
+var accountpassword = "1574269"
+accountcity = "jaipur"
+let accountstate; 
+// if you r not giving any value then its undefined 
+
+//  accountid = 4 // not allowed
+
+ accountemail = "ahsnddhd@gmail.com"
+ accountpassword = "258746"
+ accountcity = "pune"
+
+console.log(accountid); 
+
+
+/*
+perfer not to use var
+because issue in block scope and functional scope 
+*/
+
+console.table([accountid,accountemail,accountpassword,accountcity,accountstate]);
+
