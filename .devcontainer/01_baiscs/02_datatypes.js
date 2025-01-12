11319930cd6464599a8c9babe6e07f6266f5a53f
Rutuja Mirkale <146189819+rituja2911@users.noreply.github.com>
2025-01-12 19:42:02 +0000

    datatypes

---
+++ .devcontainer/01_baiscs/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) // we r using nodejs, not browser
+
+console.log(3
+    +3) // code readblity should be high
+
+ console.log("rutuja");
+
+let name = "Rutuja"
+let age = "23"
+let isloggedin = "true"
+let state;
+
+//number => 2 to power 53 
+//bigint 
+//string => ""
+//boolen => true/false 
+//null => standalone value 
+//undefined => 
+//symbol => unique 
+
+//object =>
+
+console.log(typeof "Rutuja");
+console.log(typeof null); // its object 
+console.log(typeof undefined ); // its undefined 
+    
