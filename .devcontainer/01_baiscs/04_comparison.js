04abe3f42162c67fcfe1b9484cb0ba82b715cccc
Rutuja Mirkale <146189819+rituja2911@users.noreply.github.com>
2025-01-12 21:05:43 +0000

    comparison

---
+++ .devcontainer/01_baiscs/04_comparison.js
@@ -0,0 +1,22 @@
+//console.log(2 > 1);
+//console.log(2 >= 1);
+//console.log(2 < 1);
+//console.log(2 == 1);
+//console.log(2 != 1);
+
+
+//nodconsole.log("2" > 1);
+//console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+// comparsion and equality check r differnet things 
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+//these kind of conversions are confusable and aviod these 
+
+// ===
+console.log("2" = 2);
