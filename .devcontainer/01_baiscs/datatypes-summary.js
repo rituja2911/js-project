90cca7a0e8eec9720758e5cd44b794e2f47b94c9
Rutuja Mirkale <146189819+rituja2911@users.noreply.github.com>
2025-01-13 12:16:44 +0000

    datatype summary

---
+++ .devcontainer/01_baiscs/datatypes-summary.js
@@ -0,0 +1,37 @@
+// datatypes of js there are 2 types of datatypes 
+// prmitive and non prmitive / refernce 
+// 7 types of prmitives => string, number, null , boolearn, undefined,  symbol, bigint 
+const score = 100
+const scorevalue = 100.3
+const isloggedin = true 
+const outsidetemp = null
+let useremail;
+
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+console.log(id === anotherid);
+
+const bigNumber = 26872325596332255563614782n
+
+
+
+// js is a dymic lanunage or static langnge => its a dynmic lanungae 
+
+// nonprmitive / refernce => array, objects , functions
+
+//const heros = [shatiman, superman]
+//let myObj = { 
+    name: "Rutuja" 
+    age : 23,
+//}
+
+
+const myfunction = function(){
+    console.log("Hello world "); 
+}
+
+// type of operator read pending 
+
+//console.log(typeof myfunction);
