67a36617d27a14fc78b4f566d8fa88de3708392f
Rutuja Mirkale <146189819+rituja2911@users.noreply.github.com>
2025-01-15 10:20:27 +0000

    strings

---
+++ .devcontainer/01_baiscs/05_strings.js
@@ -0,0 +1,30 @@
+const name ="Rutuja"
+const repocount = 20
+
+//console.log(name + repocount + "value");
+
+//console.log (`Hello my name is ${name} and my repo count is ${repocount}`);
+
+const gamename = new String ('rutuja-hcc-com')
+//console.log(gamename[0]);
+//console.log(gamename.__proto__);
+//console.log(gamename.length);
+//console.log(gamename.toUpperCase);
+//console.log(gamename.charAt(2));
+//console.log(gamename.indexOf('t'));
+
+const newstring = gamename.substring(0,5)
+//console.log(newstring);
+
+const anothergame = gamename. slice(-10,5)
+//console.log(anothergame);
+
+const newstringone = "      rutuja    "
+//console.log(newstringone);
+//console.log(newstringone.trim());
+
+const url = "htpps://rutuja.com/rutuja%11mirkale"
+//console.log(url.replace('%11', '-'));
+//console.log(url.includes('rutuja'));
+console.log(gamename.split('-'));
+
